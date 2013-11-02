18642c8b21e515251be9b03c4f5d438c7c0cccd8
Jo Kerkhofs <jke@thinkerit.be>
2013-11-02 18:24:33 +0100

    Initial commit

---
+++ src/image.carousel.js
@@ -0,0 +1,105 @@
+angular.module('image.carousel', [])
+    .factory('updateImageCarousel', ['config', 'scopedRestServiceHandler', 'topicMessageDispatcher', UpdateImageCarouselFactory])
+    .factory('fetchImageCarousel', ['config', 'scopedRestServiceHandler', FetchImageCarouselFactory])
+    .directive('imageCarousel', ImageCarouselDirectiveFactory);
+
+function UpdateImageCarouselFactory(config, scopedRestServiceHandler, topicMessageDispatcher) {
+    return function ($scope, carousel, success) {
+        scopedRestServiceHandler({
+            scope: $scope,
+            params: {
+                method: 'PUT',
+                url: (config.baseUri || '') + 'api/entity/image-carousel',
+                data: {
+                    namespace: config.namespace,
+                    name: carousel.name,
+                    length: carousel.length - 0
+                },
+                withCredentials: true
+            },
+            success: function () {
+                success();
+                topicMessageDispatcher.fire('system.success', {
+                    code: 'image.carousel.update.success',
+                    default: 'Image Carousel Updated!'
+                });
+            }
+        });
+    }
+}
+
+function FetchImageCarouselFactory(config, scopedRestServiceHandler) {
+    return function ($scope, name, presenter) {
+        scopedRestServiceHandler({
+            scope: $scope,
+            params: {
+                method: 'GET',
+                headers:{
+                    'x-namespace':config.namespace
+                },
+                url: (config.baseUri || '') + 'api/entity/image-carousel?name=' + name
+            },
+            success: presenter,
+            notFound: function() {
+                presenter({length:0});
+            }
+        });
+    }
+}
+
+function ImageCarouselDirectiveFactory() {
+    return {
+        restrict: ['E', 'A', 'C'],
+        scope: {
+            name: '@'
+        },
+        controller: ['$scope', 'fetchImageCarousel', 'updateImageCarousel', ImageCarouselController],
+        link: function (scope, els, attrs, ctrl) {
+            scope.$watch('name', function () {
+                ctrl.init(scope.name);
+            });
+        }
+    };
+}
+
+function ImageCarouselController($scope, fetchImageCarousel, updateImageCarousel) {
+    var self = this;
+
+    this.init = function (name) {
+        $scope.new = {};
+        $scope.carousel = {name: name};
+        $scope.editable = false;
+        fetchImageCarousel($scope, name, function (it) {
+            $scope.carousel.length = it.length;
+            self.generateItems();
+        });
+    };
+
+    this.generateItems = function () {
+        var items = [];
+        for (var i = 0; i < $scope.carousel.length; i++)
+            items.push({
+                index: i,
+                path: self.toPath(i)
+            });
+        $scope.carousel.items = items;
+    };
+
+    this.toPath = function (idx) {
+        var prefix = 'carousels';
+        if ($scope.carousel.name.lastIndexOf('/', 0) != 0) prefix += '/';
+        return prefix + $scope.carousel.name + '/' + idx + '.img';
+    };
+
+    $scope.edit = function () {
+        $scope.editable = !$scope.editable;
+    };
+
+    $scope.submit = function () {
+        $scope.carousel.length = $scope.new.length;
+        $scope.carousel.length -= 0;
+        updateImageCarousel($scope, $scope.carousel, function () {
+            self.generateItems();
+        });
+    }
+}
---
+++ test/image.carousel.spec.js
@@ -0,0 +1,314 @@
+describe('image carousel', function () {
+    beforeEach(module('image.carousel'));
+    beforeEach(module('notifications'));
+    beforeEach(module('rest.client'));
+    beforeEach(module('config'));
+
+    beforeEach(inject(function(config) {
+        config.namespace = 'namespace';
+    }));
+
+    describe('update-image-carousel service', function () {
+        var rest, success;
+        var scope = 'scope';
+        var carousel = {
+            name: 'name',
+            length: 2
+        };
+
+        beforeEach(inject(function (updateImageCarousel, scopedRestServiceHandlerMock) {
+            rest = scopedRestServiceHandlerMock;
+            success = false;
+            updateImageCarousel(scope, carousel, function() {
+                success = true;
+            });
+        }));
+
+        it('performs PUT request', inject(function (config) {
+            expect(rest.context.scope).toEqual(scope);
+            expect(rest.context.params.method).toEqual('PUT');
+            expect(rest.context.params.url).toEqual('api/entity/image-carousel');
+            expect(rest.context.params.data).toEqual({
+                namespace: config.namespace,
+                name: carousel.name,
+                length: carousel.length
+            });
+            expect(rest.context.params.withCredentials).toEqual(true);
+        }));
+
+        it('performs PUT request with baseUri', inject(function (config, updateImageCarousel, scopedRestServiceHandlerMock) {
+            config.baseUri = 'http://host/context/';
+            rest = scopedRestServiceHandlerMock;
+            success = false;
+            updateImageCarousel(scope, carousel, function() {
+                success = true;
+            });
+            expect(rest.context.params.url).toEqual(config.baseUri + 'api/entity/image-carousel');
+        }));
+
+        describe('success', function () {
+            var dispatcher;
+
+            beforeEach(inject(function (topicMessageDispatcherMock, scopedRestServiceHandlerMock) {
+                dispatcher = topicMessageDispatcherMock;
+                scopedRestServiceHandlerMock.context.success();
+            }));
+
+            it('invokes callback', function() {
+                expect(success).toEqual(true);
+            });
+
+            it('raise success notification', function () {
+                expect(dispatcher['system.success']).toEqual({
+                    code: 'image.carousel.update.success',
+                    default: 'Image Carousel Updated!'
+                });
+            });
+        });
+    });
+
+    describe('fetch-image-carousel service', function () {
+        var rest, carousel;
+        var scope = 'scope';
+        var name = "name";
+
+        beforeEach(inject(function (fetchImageCarousel, scopedRestServiceHandlerMock) {
+            rest = scopedRestServiceHandlerMock;
+            fetchImageCarousel(scope, name, function (it) {
+                carousel = it;
+            });
+        }));
+
+        it('performs GET request', inject(function (config) {
+            expect(rest.context.scope).toEqual(scope);
+            expect(rest.context.params.method).toEqual('GET');
+            expect(rest.context.params.headers['x-namespace']).toEqual(config.namespace);
+            expect(rest.context.params.url).toEqual('api/entity/image-carousel?name=' + name);
+        }));
+
+        it('performs GET request with baseUri', inject(function (config, fetchImageCarousel, scopedRestServiceHandlerMock) {
+            config.baseUri = 'http://host/context/';
+            rest = scopedRestServiceHandlerMock;
+            fetchImageCarousel(scope, name, function (it) {
+                carousel = it;
+            });
+            expect(rest.context.params.url).toEqual(config.baseUri + 'api/entity/image-carousel?name=' + name);
+        }));
+
+        describe('success', function () {
+            var payload = 'payload';
+
+            beforeEach(inject(function (scopedRestServiceHandlerMock) {
+                scopedRestServiceHandlerMock.context.success(payload);
+            }));
+
+            it('present carousel', function () {
+                expect(carousel).toEqual(payload);
+            });
+        });
+
+        describe('not found', function() {
+            beforeEach(inject(function (scopedRestServiceHandlerMock) {
+                scopedRestServiceHandlerMock.context.notFound();
+            }));
+
+            it('expose carousel with 0 length', function() {
+                expect(carousel).toEqual({length:0});
+            });
+        });
+    });
+
+    describe('image-carousel directive', function () {
+        var directive;
+
+        beforeEach(inject(function () {
+            directive = ImageCarouselDirectiveFactory();
+        }));
+
+        it('restricted on', function () {
+            expect(directive.restrict).toEqual(['E', 'A', 'C']);
+        });
+
+        it('scope', function () {
+            expect(directive.scope).toEqual({
+                name: '@'
+            });
+        });
+
+        it('controller', function () {
+            expect(directive.controller).toEqual(['$scope', 'fetchImageCarousel', 'updateImageCarousel', ImageCarouselController]);
+        });
+
+        describe('on link', function () {
+            var scope, els, attrs, ctrl, watches;
+
+            beforeEach(function () {
+                watches = {};
+                scope = {
+                    name: '/carousel/name',
+                    $watch: function (expression, callback) {
+                        watches[expression] = callback;
+                    }
+                };
+                ctrl = {init: function (name) {
+                    this.name = name;
+                }};
+                directive.link(scope, els, attrs, ctrl);
+            });
+
+            it('controller not initialized', function () {
+                expect(ctrl.name).toEqual(undefined);
+            });
+
+            describe('on triggered watch', function () {
+                beforeEach(function () {
+                    watches['name']();
+                });
+
+                it('init controller', function () {
+                    expect(ctrl.name).toEqual(scope.name);
+                });
+            });
+        });
+    });
+
+    describe('ImageCarouselController', function () {
+        var scope, ctrl, updater, fetcher, name;
+
+        beforeEach(inject(function ($controller) {
+            name = 'carousel-name';
+            scope = {};
+            updater = {};
+            fetcher = {};
+            ctrl = $controller(ImageCarouselController, {
+                $scope: scope,
+                updateImageCarousel: function (scope, carousel, success) {
+                    updater.scope = scope;
+                    updater.carousel = carousel;
+                    updater.success = success;
+                },
+                fetchImageCarousel: function (scope, name, presenter) {
+                    fetcher.scope = scope;
+                    fetcher.name = name;
+                    fetcher.presenter = presenter;
+                }
+            });
+        }));
+
+        describe('on init', function () {
+            beforeEach(function () {
+                ctrl.init(name);
+            });
+
+            it('expose name on scope', function () {
+                expect(scope.carousel.name).toEqual(name);
+            });
+
+            it('editable mode disabled', function () {
+                expect(scope.editable).toEqual(false);
+            });
+
+            describe('fetch details', function () {
+                it('passes scope', function () {
+                    expect(fetcher.scope).toEqual(scope);
+                });
+
+                it('passes name', function () {
+                    expect(fetcher.name).toEqual(name);
+                });
+
+                describe('success', function () {
+                    var payload = {length: 5};
+
+                    beforeEach(function () {
+                        fetcher.presenter(payload);
+                    });
+
+                    it('expose length on scope', function () {
+                        expect(scope.carousel.length).toEqual(payload.length);
+                    });
+
+                    it('expose carousel items on scope', function() {
+                        expect(scope.carousel.items).toEqual([
+                            {index:0, path:'carousels/carousel-name/0.img'},
+                            {index:1, path:'carousels/carousel-name/1.img'},
+                            {index:2, path:'carousels/carousel-name/2.img'},
+                            {index:3, path:'carousels/carousel-name/3.img'},
+                            {index:4, path:'carousels/carousel-name/4.img'}
+                        ]);
+                    });
+                });
+            });
+        });
+
+        describe('on init when name has a leading slash', function() {
+            beforeEach(function () {
+                name = '/' + name;
+                ctrl.init(name);
+            });
+
+            describe('success', function () {
+                var payload = {length: 1};
+
+                beforeEach(function () {
+                    fetcher.presenter(payload);
+                });
+
+                it('expose carousel items on scope', function() {
+                    expect(scope.carousel.items).toEqual([
+                        {index:0, path:'carousels/carousel-name/0.img'}
+                    ]);
+                });
+            });
+        });
+
+        describe('on edit', function () {
+            beforeEach(function () {
+                scope.edit();
+            });
+
+            it('editable mode enabled', function () {
+                expect(scope.editable).toEqual(true);
+            });
+        });
+
+        describe('on submit', function () {
+            describe('perform', function () {
+                beforeEach(function () {
+                    scope.new = {length:3};
+                    scope.carousel = {
+                        name: '/carousel/name'
+                    };
+                    scope.submit();
+                });
+
+                it('perform PUT request', function () {
+                    expect(updater.scope).toEqual(scope);
+                    expect(updater.carousel).toEqual({
+                        name: scope.carousel.name,
+                        length: scope.carousel.length
+                    });
+                });
+
+                describe('success', function() {
+                    beforeEach(function() {
+                        updater.success();
+                    });
+
+                    it('update items', function() {
+                        expect(scope.carousel.items.length).toEqual(3);
+                    });
+                });
+            });
+
+            describe('ensure length', function () {
+                it('is converted from string to int', function () {
+                    scope.new = {length:'3'};
+                    scope.carousel = {};
+                    scope.submit();
+                    expect(updater.carousel.length).toEqual(3);
+                });
+            });
+        });
+    });
+});
